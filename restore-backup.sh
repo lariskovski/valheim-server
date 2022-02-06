781e5d5a1415911f73009eeb98b524596d05de95
Larissa Porto <larissaporto@live.com>
2022-02-05 21:42:44 -0300

    Minor changes

--- restore-backup.sh
+++ restore-backup.sh
@@ -1,8 +1,8 @@
 # List all backups
 # gsutil ls -l gs://valheim-world-backup/backups | sort -k 2
-
-export BUCKET=$1
-export LATEST_BACKUP_NAME=$2
+# export BUCKET=$1
+# export LATEST_BACKUP_NAME=$2
+export LATEST_BACKUP_NAME=[add backup name]
 
 sudo apt install unzip -y
 
@@ -10,5 +10,5 @@ sudo apt install unzip -y
 sudo su steam
 [ -d /home/steam/valheim/config/ ] || mkdir /home/steam/valheim/config/
 
-sudo gsutil cp $BUCKET$LATEST_BACKUP_NAME $HOME
+# sudo gsutil cp $BUCKET$LATEST_BACKUP_NAME $HOME
 sudo unzip $LATEST_BACKUP_NAME -d /home/steam/valheim/config/
--- valheim-setup.sh
+++ valheim-setup.sh
@@ -3,13 +3,17 @@ sudo timedatectl set-timezone America/Sao_Paulo
 sudo apt update
 
 sudo useradd steam --create-home --shell /bin/bash
-sudo usermod -aG google-sudoers steam
+# sudo usermod -aG google-sudoers steam
 
 mkdir -p /home/steam/valheim
+sudo chown steam:steam -R /home/steam
 
 curl https://get.docker.com | sh
 sudo systemctl enable docker
 sudo systemctl start docker
+
+sudo usermod -aG sudo steam
+sudo usermod -aG docker steam
 
 # For a restore get the latest backup name on bucket
 # and uncomment the lines below:
@@ -21,12 +25,13 @@ sudo docker run -d --restart always\
     --cap-add=sys_nice \
     --stop-timeout 120 \
     -p 2456-2457:2456-2457/udp \
+    -v /home/steam/valheim/restore/worlds:/config/worlds \
     -v /home/steam/valheim/config:/config \
     -v /home/steam/valheim/data:/opt/valheim \
     -e SERVER_NAME="No Naked Vikings Allowed" \
     -e WORLD_NAME="The Vik Zon" \
-    -e SERVER_PASS="arthursucks" \
+    -e SERVER_PASS="" \
     lloesche/valheim-server
 
-(crontab -l ; echo "0,30 * * * * gsutil -m rsync -d -r /home/steam/valheim/config/backups gs://valheim-world-backup/backups-sp &>> /home/steam/valheim/cron-backup.logs") | crontab -
+# (crontab -l ; echo "0,30 * * * * gsutil -m rsync -d -r /home/steam/valheim/config/backups gs://valheim-world-backup/backups-sp &>> /home/steam/valheim/cron-backup.logs") | crontab -
 
