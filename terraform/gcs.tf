25d62050f7223a217864dff86238fa0304cba9ff
Larissa <37162993+lariskovski@users.noreply.github.com>
2024-08-10 20:31:23 -0300

    Fix/gce image (#5)
    * fix(gce): fix image
    
    * fix(network): enable public ip
    
    * fix(gcs): fix gcs permissions and script
    
    * fix(gcs): bucket name fix
    
    * fix(gcs): bucket permission role name

--- terraform/gcs.tf
+++ terraform/gcs.tf
@@ -1,4 +1,13 @@
+resource "google_storage_bucket_access_control" "default" {
+  bucket = google_storage_bucket.default.name
+  role   = "WRITER"
+  entity = google_service_account.default.email
+}
+
 resource "google_storage_bucket" "default" {
-    name     = var.application
-    location = "US"
+    name            = "valheim-server-ashlands"
+    location        = "US-CENTRAL1"
+    storage_class   = "STANDARD"
+
+    uniform_bucket_level_access = true
 }
