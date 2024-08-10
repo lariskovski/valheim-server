b0ffe9c2620e5071c70bd49c9a67d745f9c9ad97
Larissa <37162993+lariskovski@users.noreply.github.com>
2024-08-10 23:59:23 +0000

    fix(gcs): add depends on for iam policy/sa/bucket order

--- terraform/gcs.tf
+++ terraform/gcs.tf
@@ -1,21 +1,28 @@
 data "google_iam_policy" "default" {
-  binding {
-    role = "roles/storage.admin"
-    members = [
-      "serviceAccount:${google_service_account.default.email}",
+    binding {
+        role = "roles/storage.admin"
+        members = [
+        "serviceAccount:${google_service_account.default.email}",
+        ]
+    }
+    depends_on = [
+        google_service_account.default
     ]
-  }
-}
-
-resource "google_storage_bucket_iam_policy" "policy" {
-  bucket = google_storage_bucket.default.name
-  policy_data = data.google_iam_policy.default.policy_data
 }
 
 resource "google_storage_bucket" "default" {
+    # name is fixed because startup.sh depends on it
     name            = "valheim-server-ashlands"
     location        = "US-CENTRAL1"
     storage_class   = "STANDARD"
 
     uniform_bucket_level_access = true
+}
+
+resource "google_storage_bucket_iam_policy" "policy" {
+    bucket = google_storage_bucket.default.name
+    policy_data = data.google_iam_policy.default.policy_data
+    depends_on = [
+    google_storage_bucket.default
+  ]
 }
