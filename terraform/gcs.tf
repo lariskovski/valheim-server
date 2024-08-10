ab0889ed6cf291b020df22769f718f5edcd84394
Larissa <37162993+lariskovski@users.noreply.github.com>
2024-08-10 23:30:57 +0000

    fix(gcs): bucket permission role name

--- terraform/gcs.tf
+++ terraform/gcs.tf
@@ -1,6 +1,6 @@
 resource "google_storage_bucket_access_control" "default" {
   bucket = google_storage_bucket.default.name
-  role   = "WRITE"
+  role   = "WRITER"
   entity = google_service_account.default.email
 }
 
